00bff1a43d8ff4f79804f03d897d67a7ac1b7d08
mooniflow <anscks2808@naver.com>
2025-02-12 17:15:43 +0900

    v3

--- src/main/java/com/hello/demo/entity/User.java
+++ src/main/java/com/hello/demo/entity/User.java
@@ -4,45 +4,40 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
-@Entity(name="users")
+@Entity(name = "users")
+@Getter
+@Setter
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String username;
+
+    private String email;
+
     private String password;
-    
-    // 생성자, Getter, Setter
+
+    private String phone;
+
+    private String address;
+
+    private String role;
+
+    // 기본 생성자
     public User() {}
 
-    public User(String username, String password) {
+    // 매개변수가 있는 생성자
+    public User(String username, String email, String password, String phone, String address, String role) {
         this.username = username;
+        this.email = email;
         this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+        this.phone = phone;
+        this.address = address;
+        this.role = role;
     }
 }
